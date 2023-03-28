21cb8026deba2d97a08f295f9aec675c3081833e
jarfernandez <jarfernandez@gmail.com>
2023-03-29 01:44:45 +0200

    Se modifica la salida de la aplicación web

--- WebApp/webapp.py
+++ WebApp/webapp.py
@@ -5,7 +5,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return f"""
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>Hello, World!</title>
+    </head>
+    <body>
+      <h1>Hello, World!</h1>
+    </body>
+    </html>
+    """
 
 
 if __name__ == "__main__":
